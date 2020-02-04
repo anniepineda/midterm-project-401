229a21086522d3d7430b924b6641c6d45e8ad555
jjdeforrest <jjdeforrest@users.noreply.github.com>
2020-02-04 14:18:10 -0800

    html update

--- memestagram/src/main/java/io/romellpineda/memestagram/controllers/HomeController.java
+++ memestagram/src/main/java/io/romellpineda/memestagram/controllers/HomeController.java
@@ -3,7 +3,15 @@ package io.romellpineda.memestagram.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.security.Principal;
 
 @Controller
@@ -15,5 +23,42 @@ public class HomeController {
             m.addAttribute("username", p.getName());
         }
         return "index";
+    }
+
+    @Controller
+    public static class MemeController {
+
+        boolean displayDiv= false;
+
+        @GetMapping("/generator")
+        public String getToGenerator(Model m, boolean a) {
+            m.addAttribute("displayDiv", a);
+            return "generator";
+        }
+
+        @PostMapping("/generator")
+        public RedirectView generateMeme(String linkMeme, String textToAdd, String name, Model m) throws IOException {
+            final BufferedImage image = ImageIO.read(new URL(linkMeme));
+            if (image == null) {
+                displayDiv=true;
+                m.addAttribute("displayDiv", true);
+                return new RedirectView("/generator?a=true");
+            }
+            Graphics g = null;
+
+            if (image != null) {
+                g = image.getGraphics();
+            }
+            g.setFont(g.getFont().deriveFont(30f));
+            g.drawString(textToAdd, 100, 100);
+            g.dispose();
+
+            try {
+                ImageIO.write(image, "png", new File(name+".png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return new RedirectView("/generator");
+        }
     }
 }
